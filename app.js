55f17cb8c131d51a9da2023c73df0b342c802553
Naved Fakru <naved10055@gmail.com>
2023-06-29 22:47:33 +0530

    Level -3 completed

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY)
+console.log(md5("123456"))
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -23,8 +23,6 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 // level - 2 encryption
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("User", userSchema)
@@ -46,7 +44,7 @@ app.post("/register", async function(req, res) {
     try {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
 
         await newUser.save();
@@ -59,7 +57,7 @@ app.post("/register", async function(req, res) {
 app.post("/login", async function(req, res) {
     try {
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         const foundUser = await User.findOne({ email: username });
 
